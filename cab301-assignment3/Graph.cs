0ee9d2cd4e7072d8ffcaebb9b80340897616f609
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2023-05-24 19:17:57 +1000

    Read from file into dictionary

---
+++ cab301-assignment3/Graph.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cab301_assignment3
+{
+    internal class Graph
+    {
+        private Dictionary<string, List<string>> adjacencyList;
+
+        public Graph()
+        {
+            adjacencyList = new Dictionary<string, List<string>>();
+        }
+
+        public void ReadTasksFromFile(string filePath)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(',');
+
+                    if (parts.Length >= 2)
+                    {
+                        string taskId = parts[0].Trim();
+                        int timeNeeded;
+
+                        if (int.TryParse(parts[1].Trim(), out timeNeeded))
+                        {
+                            List<string> dependencies = new List<string>();
+
+                            for (int i = 2; i < parts.Length; i++)
+                            {
+                                dependencies.Add(parts[i].Trim());
+                            }
+
+                            AddTask(taskId, timeNeeded, dependencies);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Invalid time needed for task '{taskId}'");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid line: {line}");
+                    }
+                }
+
+                Console.WriteLine("Tasks and dependencies loaded from file successfully!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error while reading the file: " + e.Message);
+            }
+        }
+
+        public void AddTask(string taskId, int timeNeeded, List<string> dependencies)
+        {
+            if (!adjacencyList.ContainsKey(taskId))
+            {
+                adjacencyList.Add(taskId, dependencies);
+            }
+            else
+            {
+                Console.WriteLine($"Task '{taskId}' already exists in the graph.");
+            }
+        }
+
+        public void PrintTasks()
+        {
+            Console.WriteLine("Tasks in the graph:");
+            foreach (var task in adjacencyList.Keys)
+            {
+                Console.WriteLine(task);
+            }
+        }
+    }
+}
--- cab301-assignment3/Program.cs
+++ cab301-assignment3/Program.cs
@@ -1,8 +1,12 @@
-﻿Console.WriteLine("╔═══════════════════════════════╗");
+﻿using cab301_assignment3;
+
+
+Console.WriteLine("╔═══════════════════════════════╗");
 Console.WriteLine("║     Welcome to TaskMinder     ║");
 Console.WriteLine("╚═══════════════════════════════╝");
 Console.WriteLine();
 
+Graph graph = new Graph();
 string filePath = "";
 
 bool exit = false;
@@ -16,7 +20,7 @@ while (!exit)
         switch (choice)
         {
             case 1:
-                InitialiseFile();
+                InitialiseFile(graph);
                 break;
             case 8:
                 exit = true;
@@ -34,51 +38,34 @@ while (!exit)
 void DisplayMenu()
 {
     Console.WriteLine("Menu:");
-    Console.WriteLine("1. Load Project from File");
-    Console.WriteLine("2. Add Job");
-    Console.WriteLine("3. Remove Job");
-    Console.WriteLine("4. Change Job Time");
-    Console.WriteLine("5. Save Project to File");
-    Console.WriteLine("6. Find Task Sequence");
-    Console.WriteLine("7. Find Earliest Times");
-    Console.WriteLine("8. Exit");
+    Console.WriteLine("(1) Load Project from File");
+    Console.WriteLine("(2) Add Job");
+    Console.WriteLine("(3) Remove Job");
+    Console.WriteLine("(4) Change Job Time");
+    Console.WriteLine("(5) Save Project to File");
+    Console.WriteLine("(6) Find Task Sequence");
+    Console.WriteLine("(7) Find Earliest Times");
+    Console.WriteLine("(8) Exit");
     Console.Write("Enter your choice: ");
 }
 
-void InitialiseFile()
+static void InitialiseFile(Graph graph)
 {
     Console.Write("Enter full path of the text file (with extension): ");
-    filePath = Console.ReadLine();
+    string filePath = Console.ReadLine();
 
     if (!File.Exists(filePath))
     {
         Console.WriteLine("File '{0}' does not exist", filePath);
-        filePath = "";
     }
     else if (Path.GetExtension(filePath) != ".txt")
     {
         Console.WriteLine("File '{0}' is not a .txt file", filePath);
-        filePath = "";
     }
     else
     {
         Console.WriteLine("File '{0}' loaded successfully!", filePath);
-
+        graph.ReadTasksFromFile(filePath);
+        graph.PrintTasks();
     }
-}
-
-try
-{
-    //Pass the filepath and filename to the StreamWriter Constructor
-    StreamWriter sw = new StreamWriter(filePath);
-    //Write a line of text
-    sw.WriteLine("Hello World!!");
-    //Write a second line of text
-    sw.WriteLine("From the StreamWriter class");
-    //Close the file
-    sw.Close();
-}
-catch (Exception e)
-{
-    Console.WriteLine("Exception: " + e.Message);
 }
